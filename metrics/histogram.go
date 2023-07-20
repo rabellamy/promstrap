c53af771f3fb7687eba96d1ea602986d8c8e8dce
Robert Anthony Bellamy <rabellamy@gmail.com>
2023-07-20 07:25:32 -0400

    adds Buckets to histogram opts and Objectives to summary opts

--- metrics/histogram.go
+++ metrics/histogram.go
@@ -8,6 +8,9 @@ type HistogramOpts struct {
 	Name      string   `validate:"required"`
 	Help      string   `validate:"required"`
 	Labels    []string `validate:"required"`
+	// Buckets defines the buckets into which observations are counted. Each
+	// element in the slice is the upper inclusive bound of a bucket.
+	Buckets []float64
 }
 
 // NewHistogramWithLabels creates a Prometheus histogram with labels based on the
@@ -15,15 +18,20 @@ type HistogramOpts struct {
 // A histogram samples observations (usually things like request durations or
 // response sizes) and counts them in configurable buckets. It also provides
 // a sum of all observed values.
-// TODO: make Objectives configurable.
 func NewHistogramWithLabels(opts HistogramOpts) (*prometheus.HistogramVec, error) {
 	if err := Validate(opts); err != nil {
 		return nil, err
 	}
 
-	return prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	pOpts := prometheus.HistogramOpts{
 		Namespace: opts.Namespace,
 		Help:      opts.Help,
 		Name:      opts.Name,
-	}, opts.Labels), nil
+	}
+
+	if opts.Buckets != nil {
+		pOpts.Buckets = opts.Buckets
+	}
+
+	return prometheus.NewHistogramVec(pOpts, opts.Labels), nil
 }
--- metrics/summary.go
+++ metrics/summary.go
@@ -8,6 +8,9 @@ type SummaryOpts struct {
 	Name      string   `validate:"required"`
 	Help      string   `validate:"required"`
 	Labels    []string `validate:"required"`
+	// Objectives defines the quantile rank estimates with their respective
+	// absolute error.
+	Objectives map[float64]float64
 }
 
 // NewSummaryWithLabels creates a Prometheus summary with labels based on the
@@ -16,15 +19,20 @@ type SummaryOpts struct {
 // response sizes). While it also provides a total count of observations and a
 // sum of all observed values, it calculates configurable quantiles over
 // a sliding time window.
-// TODO: make Buckets and NativeHistogramBucketFactor configurable.
 func NewSummaryWithLabels(opts SummaryOpts) (*prometheus.SummaryVec, error) {
 	if err := Validate(opts); err != nil {
 		return nil, err
 	}
 
-	return prometheus.NewSummaryVec(prometheus.SummaryOpts{
+	pOpts := prometheus.SummaryOpts{
 		Namespace: opts.Namespace,
 		Help:      opts.Help,
 		Name:      opts.Name,
-	}, opts.Labels), nil
+	}
+
+	if opts.Objectives != nil {
+		pOpts.Objectives = opts.Objectives
+	}
+
+	return prometheus.NewSummaryVec(pOpts, opts.Labels), nil
 }
