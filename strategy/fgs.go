72c5f1faac5513e0c201de6cc17760bb2e474cf4
Nazmus Sarwar <nazmus.sarwar@alticeusa.com>
2025-02-25 04:21:18 -0500

    fix: Add back comment that where removed accidentally for fgs

--- strategy/fgs.go
+++ strategy/fgs.go
@@ -10,9 +10,23 @@ import (
 // If you can only measure four metrics of your user-facing system, focus on these four.
 // https://sre.google/sre-book/monitoring-distributed-systems/
 type FourGoldenSignals struct {
-	Latency    *Distribution
-	Traffic    *prometheus.CounterVec
-	Errors     *prometheus.CounterVec
+	// Latency is the time it takes to service a request. It’s important to
+	// distinguish between the latency of successful requests and the latency of failed
+	// requests.
+	Latency *Distribution
+	// Traffic is a measure of how much demand is being placed on your system,
+	// measured in a high-level system-specific metric. For a web service, this
+	// measurement is usually HTTP requests per second, perhaps broken out by the
+	// nature of the requests (e.g., static versus dynamic content).
+	Traffic *prometheus.CounterVec
+	// Errors is the rate of requests that fail, either explicitly (e.g., HTTP 500s),
+	// implicitly (for example, an HTTP 200 success response, but coupled with the wrong content)
+	Errors *prometheus.CounterVec
+	// Saturation is How "full" your service is. A measure of your system fraction,
+	// emphasizing the resources that are most constrained. The degree to which extra
+	// work is queued (or denied) that can't be serviced (e.g., in a memory-constrained system,
+	// show memory; in an I/O-constrained system, show I/O or another example could be
+	// scheduler run queue length).
 	Saturation *prometheus.GaugeVec
 
 	opts FourGoldenSignalsOpts
